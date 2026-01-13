a98640e57d6e73f6b53a5294c0000136193a2f9f
paulosmb1972 <paulosmb1972@gmail.com>
2026-01-13 11:30:58 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,6 @@
+window.eleicaoData = {
+    nomeEleicao: "Eleição 2024",
+    candidatos: [
+        { numero: "10", nome: "Candidato Teste", partido: "P1", foto: "https://via.placeholder.com/150" }
+    ]
+};
