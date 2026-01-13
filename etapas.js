0c5645834ac45e7ce11e78f2895b3dd4b681395a
paulosmb1972 <paulosmb1972@gmail.com>
2026-01-13 12:51:10 -0300

    Update etapas.js

--- etapas.js
+++ etapas.js
@@ -1,16 +1,20 @@
-let etapas = [
-    {
-        titulo: 'VEREADOR',
-        numeros: 5,
-        candidatos: [
-            {
-                numero: '38111',
-                nome: 'CANDIDATO TESTE',
-                partido: 'ABC',
-                fotos: [
-                    {url: 'https://via.placeholder.com/150', legenda: 'Vereador'}
-                ]
-            }
-        ]
-    }
-];
+window.config = {
+    nomeSistema: "UrnaWeb",
+    etapas: [
+        {
+            titulo: 'VEREADOR',
+            numeros: 5,
+            candidatos: [
+                {
+                    numero: '38111',
+                    nome: 'CANDIDATO TESTE',
+                    partido: 'PARTIDO TESTE',
+                    fotos: [{url: 'https://via.placeholder.com/150', legenda: 'Vereador'}]
+                }
+            ]
+        }
+    ]
+};
+
+// Por garantia, se o script procurar por 'etapas' diretamente:
+let etapas = window.config.etapas;
